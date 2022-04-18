198da5b43d7de9493bcaa5625933edd1a5100e61
Qibin <qbye0405@gmail.com>
2022-04-18 02:33:54 -0400

    commit tree 7665e7d87556cc23ff3b01929f50c0a0d46dc936

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+//remove element named main from the DOM
+//using the function named document.querySelector()
+const removeMain = document.querySelector('main#main');
+removeMain.remove();
